f657edcc1f053cbca6d004da67891e631bfd9df0
Madina Akramova <madakr@outlook.com>
2023-10-19 21:09:25 +0200

    extend cypress test

--- cypress/integration/hello cypress.spec.js
+++ cypress/integration/hello cypress.spec.js
@@ -2,5 +2,12 @@
 Cypress.on('uncaught:expection', () => false);
 it('should opem web site', { scrollBehavior: false },  ()=>{
     cy.visit('https://www.cypress.io');
-
+    //cy.get('.footer-form > .border').contains(" Subscribe to our newsletter " ).click({force:true});
+    cy.get('[href*="/subscribe"]').click({force:true});
+    cy.wait(500);
+    cy.get('#subscribe_email').type('madakr@outlook.com', {delay:200});
+    //cy.get('form.relative > .gap-\[16px\] > .border').contains("Subscribe today ").click({force:true});
+    //cy.get('button[type="Subscribe today "]').as('submitBtn');
+    cy.get('button').contains("Subscribe today ").click({force:true});
+    cy.get('.w-full > .font-primary').should('have.text', 'Thank you for subscribing!');
 });
